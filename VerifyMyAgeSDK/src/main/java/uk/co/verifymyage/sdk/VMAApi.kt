a2d6319c7c85de40701ae324bab567c52b042c0b
Douglas Santos <douglas.santos.dev@gmail.com>
2020-08-18 15:19:04 -0300

    Feature/integrate with face hash endpoint (#1)
    * integrate with face hash endpoint
    
    * improving comments
    
    * first commit
    
    * dependency on maven
    
    Co-authored-by: Douglas Santos <douglassantos@MacBook-Air-de-Douglas.local>

--- VerifyMyAgeSDK/src/main/java/uk/co/verifymyage/sdk/VMAApi.kt
+++ VerifyMyAgeSDK/src/main/java/uk/co/verifymyage/sdk/VMAApi.kt
@@ -1,13 +1,22 @@
 package uk.co.verifymyage.sdk
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.annotation.RequiresApi
 import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
 import java.io.DataOutputStream
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.*
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
+import java.util.Base64
+
+
 
 class VMAApi(apiId: String, apiKey: String, apiSecret: String) {
     private var url :String = "https://api-dot-verifymyage.appspot.com"
@@ -41,22 +50,19 @@ class VMAApi(apiId: String, apiKey: String, apiSecret: String) {
         return "$apiId:$time:$hmac"
     }
 
-    fun request(method: String, endpoint: String, customer: VmaCustomer): JSONObject {
+    fun verifications(customer: VmaCustomer): JSONObject {
         val response = StringBuilder()
-        val url = URL("$url$endpoint")
+        val url = URL("$url/verifications")
         val postData: ByteArray = customer.toJsonString().toByteArray()
 
         with(url.openConnection() as HttpURLConnection){
-            requestMethod = method
+            requestMethod = "POST"
             connectTimeout = 300000
             connectTimeout = 300000
             doOutput = true
 
             setRequestProperty("Accept", "application/json")
             setRequestProperty("Content-Type", "application/json")
-            println("=============================")
-            println(customer.id)
-            println("=============================")
             setRequestProperty("Authorization", hmacHeader(customer.id))
             try {
                 val outputStream = DataOutputStream(outputStream)
@@ -82,7 +88,7 @@ class VMAApi(apiId: String, apiKey: String, apiSecret: String) {
                     }
 
                 }
-                println("============= RESPONSE FROM [POST] $endpoint ===================")
+                println("============= RESPONSE FROM [POST] $url ===================")
                 println(response.toString())
                 println("===============================================================\n")
             } catch (exception: Exception) {
@@ -93,4 +99,74 @@ class VMAApi(apiId: String, apiKey: String, apiSecret: String) {
 
         return JSONObject(response.substring(response.indexOf("{"), response.lastIndexOf("}") + 1))
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun faceUpdate(hash: String, bitmap: Bitmap) {
+        val response = StringBuilder()
+        val url = URL("$url/verifications/$hash/face")
+
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val imageString: String = Base64.getEncoder().encodeToString(baos.toByteArray())
+
+        val postData = "{ \"image\": \"$imageString\" }".toByteArray()
+
+        with(url.openConnection() as HttpURLConnection){
+            requestMethod = "PUT"
+            connectTimeout = 300000
+            doOutput = true
+
+            setRequestProperty("Accept", "application/json")
+            setRequestProperty("Content-Type", "application/json")
+            setRequestProperty("Authorization", hmacHeader(hash))
+            try {
+                val outputStream = DataOutputStream(outputStream)
+                outputStream.write(postData)
+                outputStream.flush()
+            } catch (exception: Exception) {
+                println("Exception while posting data ${exception.message}")
+            }
+            println("Response code: $responseCode")
+
+            var inputStream = errorStream
+            if (errorStream == null) {
+                inputStream = getInputStream()
+            }
+
+            try{
+                BufferedReader(
+                    InputStreamReader(inputStream, "utf-8")
+                ).use { br ->
+                    var responseLine: String? = null
+                    while (br.readLine().also { responseLine = it } != null) {
+                        response.append(responseLine!!.trim { it <= ' ' })
+                    }
+
+                }
+                println("============= RESPONSE FROM [PUT] $url ===================")
+                println(response.toString())
+                println("===============================================================\n")
+            } catch (exception: Exception) {
+                println("Exception while reading data ${exception.message}")
+
+            }
+        }
+    }
+
+    fun getBase64FromFile(path: String?): ByteArray {
+        var bmp: Bitmap? = null
+        var baos: ByteArrayOutputStream? = null
+        var byteArr = ByteArray(8192)
+        try {
+            bmp = BitmapFactory.decodeFile(path)
+            baos = ByteArrayOutputStream()
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            byteArr = baos.toByteArray()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+        return byteArr
+    }
+
 }
--- VerifyMyAgeSDK/src/main/java/uk/co/verifymyage/sdk/VerifyMyAgeVerificationFlow.kt
+++ VerifyMyAgeSDK/src/main/java/uk/co/verifymyage/sdk/VerifyMyAgeVerificationFlow.kt
@@ -1,16 +1,20 @@
 package uk.co.verifymyage.sdk
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
 import android.text.Html
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 public final class VerifyMyAgeVerificationFlow : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.verifying_age_screen)
@@ -35,9 +39,16 @@ public final class VerifyMyAgeVerificationFlow : AppCompatActivity() {
                 intent.getStringExtra("email").toString(),
                 intent.getStringExtra("phone").toString()
             );
-            var response = vmaApi.request("POST", "/verifications", customer)
+            var response = vmaApi.verifications(customer)
+
+            //TODO:
+            // 1. it is here just for tests, remove it from here and use it after face recognized
+            // 2. Replace R.drawable.face_test with the face detected image
+            val bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.face_test)
+            vmaApi.faceUpdate(response.getString("hash"), bitmap)
+
             val intent = Intent(this, VerificationsResultActivity::class.java).apply {
-                putExtra("REAUTHENTICATE", response.getString("reauthenticate"))
+                putExtra("REAUTHENTICATE", response.getBoolean("reauthenticate"))
                 putExtra("CLIENT_ID", response.getString("client_id"))
                 putExtra("STATUS", response.getString("status"))
                 putExtra("URL", response.getString("url"))
